2dc61b6a0d4e1b7af15f049b25f9c7aa1823c6c1
Riya Halder <riya.cse.gmit.2125@gmail.com>
2026-06-06 00:22:36 +0530

    Configure Render deployment

--- src/main/java/com/jobportal/backend/Application.java
+++ src/main/java/com/jobportal/backend/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JobportalAiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JobportalAiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
